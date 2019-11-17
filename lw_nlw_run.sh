143033b54b4a87763e5800aa3e26e3c7c883726f
Savannah Rogers <savannahrogers@Savannahs-MacBook-Pro.local>
2019-11-17 15:26:14 -0800

    updated directory

--- lw_nlw_run.sh
+++ lw_nlw_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mkdir ./lw_nlw_output;
-mkdir ./lw_nlw_output/lw;
-mkdir ./lw_nlw_output/nlw;
-for ((i=1; i<=100; i++)); do
+mkdir ./InputOutputStuff/lw_nlw_output;
+mkdir ./InputOutputStuff/lw_nlw_output/lw;
+mkdir ./InputOutputStuff/lw_nlw_output/nlw;
+for ((i=1; i<=3; i++)); do
     echo "Evaluating LW: $i";
-    mkdir ./lw_nlw_output/lw/lw.$i;
-    python3 GP_Main.py "./input_data/lw.csv" "./lw_nlw_output/lw/lw.$i";
+    mkdir ./InputOutputStuff/lw_nlw_output/lw/lw.$i;
+    python3 GP_Main.py "./InputOutputStuff/lw.csv" "./InputOutputStuff/lw_nlw_output/lw/lw.$i";
     echo "Evaluating NLW: $i";
-    mkdir ./lw_nlw_output/nlw/nlw.$i;
-    python3 GP_Main.py "./input_data/nlw.csv" "./lw_nlw_output/nlw/nlw.$i";
+    mkdir ./InputOutputStuff/lw_nlw_output/nlw/nlw.$i;
+    python3 GP_Main.py "./InputOutputStuff/nlw.csv" "./InputOutputStuff/lw_nlw_output/nlw/nlw.$i";
 done
